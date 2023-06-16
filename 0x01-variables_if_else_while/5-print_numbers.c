623a6d17476903957d56d76ea924570a38373065
joshuaquaye96 <joshuaamarhquaye@gmail.com>
2023-06-16 16:02:19 -0700

    base 10 print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 10; p++)
+		printf("%d", p);
+	printf("\n");
+	return (0);
+}
