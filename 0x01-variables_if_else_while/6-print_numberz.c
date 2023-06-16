2e9d5bf5746c843e542e29230ac6ee7c8383290b
joshuaquaye96 <joshuaamarhquaye@gmail.com>
2023-06-16 16:14:18 -0700

    using putchar 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 48; q < 58; q++)
+	{
+		putchar(q);
+	}
+	putchar('\n');
+	return (0);
+}
