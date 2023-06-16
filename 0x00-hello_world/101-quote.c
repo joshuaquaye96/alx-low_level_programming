2af283b49a26f79c8ac7c80c5dfde3ec62550580
joshuaquaye96 <joshuaamarhquaye@gmail.com>
2023-06-15 17:47:09 -0700

    101-quote task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
